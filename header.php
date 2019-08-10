8e03156872c24e7b0660333a137e92466074d8cf
ikodulac <elleaechapequoi@gmail.com>
2019-08-10 18:44:04 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--Document de travail, premier jet-->
+<html>
+ <head>
+  <title>Atelier Vélo-Cité</title>
+  <meta name="description" content="info sur l’atelier de vélo communautaire de Rimouski">
+  <meta name="keywords" content="velo, bike, bicyclette, atelier, communautaire, Rimouski, réparation, entretien, DIY">
+  <meta name="author" content="iko">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="test.css" />
+  <style>
+  </style>
+ </head>
+ <body>
+  <header><img src="/img/rim_header.gif" alt="logo Atelier Vélo"></header>
+  <nav>
+    <a href="index.php" title="Page titre">Vélocité&#8209;Rimouski.org</a>
+    <a href="rimbenevoles.php" title="benevoles">S'impliquer</a>
+    <!--<a class="head" href="rimforum.php" target="_blank" title="forum">Forum&nbsp;des&nbsp;membres</a>-->
+    <a href="rimformations.php" title="formations" target="_self">Formations&nbsp;mécanique vélo</a>
+    <a href="rimcontacts.php" title="contacts" target="_self">Nous&nbsp;contacter</a>
+  </nav>
